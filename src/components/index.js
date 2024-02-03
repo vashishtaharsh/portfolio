d581fcd07e9c51066f4323912c776659b4a79b27
Harsh Vashishta <harshvashishta7@gmail.com>
2024-02-04 02:23:50 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,20 @@
+import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
+import Hero from './Hero';
+import Navbar from './Navbar';
+import About from './About';
+import Tech from './Tech';
+import Experience from './Experience';
+import Contact from './Contact';
+
+export {
+  Hero,
+  Navbar,
+  About,
+  Tech,
+  Experience,
+  Contact,
+  EarthCanvas, 
+  BallCanvas, 
+  ComputersCanvas, 
+  StarsCanvas
+}
